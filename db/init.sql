7ce87c91872f3794e693e351e07dd467f89f8b87
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-05-05 18:27:55 +0900

    init.sqlにテーブル定義を追加

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,16 @@
 CREATE TABLE users (
     id uuid PRIMARY KEY,
     name varchar(255) NOT NULL,
-    nickname varchar(255)
+    nickname varchar(255),
+    created_at timestamp NOT NULL,
 );
+
+CREATE TABLE nums (
+    aaa smallint NOT NULL,
+    bbb integer NOT NULL,
+    ccc bigint NOT NULL,
+    ddd smallserial NOT NULL,
+    eee serial NOT NULL,
+    fff bigserial NOT NULL
+);
+
