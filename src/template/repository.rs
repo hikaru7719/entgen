d7eb5ee2cb3b390a730a0226086d76aa655c1243
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-05-14 15:41:31 +0900

    find_by_idに対応

--- src/template/repository.rs
+++ src/template/repository.rs
@@ -134,6 +134,15 @@ impl UsersRepository {
             .await?)
     }
 
+    pub async fn find_by_id(&self, id: &sqlx::types::Uuid) -> Result<Users, sqlx::Error> {
+        Ok(
+            sqlx::query_as::<_, Users>("SELECT * FROM users WHERE id = $1")
+                .bind(id)
+                .fetch_one(self.pool.deref())
+                .await?,
+        )
+    }
+
     pub async fn delete(&self, id: &sqlx::types::Uuid) -> Result<(), sqlx::Error> {
         sqlx::query("DELETE FROM users WHERE id = $1")
             .bind(id)
