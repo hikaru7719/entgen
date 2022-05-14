a55f0dafb8224ace525935505ea54182122817a3
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-05-14 14:27:27 +0900

    delete関数に対応

--- src/template/repository.rs
+++ src/template/repository.rs
@@ -8,6 +8,7 @@ use askama::Template;
 pub struct RepositoryTemplate {
     entity_name: String,
     fields: Vec<Field>,
+    primary_key: PrimaryKey,
 }
 
 #[derive(Debug)]
@@ -17,14 +18,39 @@ pub struct Field {
     nullable: bool,
 }
 
+#[derive(Debug)]
+pub struct PrimaryKey {
+    field_name: String,
+    field_type: String,
+    is_null: bool,
+}
+
 pub fn from_vec(
     table_name: &String,
     db_models: &Vec<information_schema::Columns>,
+    pk: &information_schema::PrimaryKey,
 ) -> RepositoryTemplate {
     let fields: Vec<Field> = db_models.iter().map(|db_model| from(db_model)).collect();
+    let primary_key = fields
+        .iter()
+        .filter(|f| *f.field_name == pk.column_name)
+        .nth(1)
+        .map_or(
+            PrimaryKey {
+                field_name: "".to_string(),
+                field_type: "".to_string(),
+                is_null: true,
+            },
+            |f| PrimaryKey {
+                field_name: f.field_name.clone(),
+                field_type: f.field_type.clone(),
+                is_null: false,
+            },
+        );
     RepositoryTemplate {
         entity_name: table_name.clone(),
         fields: fields,
+        primary_key: primary_key,
     }
 }
 
@@ -72,6 +98,81 @@ mod test {
                     nullable: true,
                 },
             ],
+            primary_key: PrimaryKey {
+                field_name: "id".to_string(),
+                field_type: "sqlx::types::Uuid".to_string(),
+                is_null: false,
+            },
+        };
+        assert_eq!(
+            template.build().unwrap(),
+            r#"use std::ops::Deref;
+use std::sync::Arc;
+
+pub struct UsersRepository {
+    pool: Arc<sqlx::PgPool>,
+}
+
+impl UsersRepository {
+    pub fn new(pool: Arc<sqlx::PgPool>) -> Self {
+        UsersRepository { pool: pool }
+    }
+
+    pub async fn insert(&self, users: &Users) -> Result<(), sqlx::Error> {
+        sqlx::query("INSERT INTO users (id, name, nickname) VALUES ($1, $2, $3)")
+            .bind(users.id)
+            .bind(users.name.clone())
+            .bind(users.nickname.as_ref())
+            .execute(self.pool.deref())
+            .await?;
+        Ok(())
+    }
+
+    pub async fn find_all(&self) -> Result<Vec<Users>, sqlx::Error> {
+        Ok(sqlx::query_as::<_, Users>("SELECT * FROM users")
+            .fetch_all(self.pool.deref())
+            .await?)
+    }
+
+    pub async fn delete(&self, id: &sqlx::types::Uuid) -> Result<(), sqlx::Error> {
+        sqlx::query("DELETE FROM users WHERE id = $1")
+            .bind(id)
+            .execute(self.pool.deref())
+            .await?;
+        Ok(())
+    }
+}
+"#
+            .to_string()
+        );
+    }
+
+    #[test]
+    fn test_build_primary_key_does_not_exists() {
+        let template = RepositoryTemplate {
+            entity_name: "users".to_string(),
+            fields: vec![
+                Field {
+                    field_name: "id".to_string(),
+                    field_type: "sqlx::types::Uuid".to_string(),
+                    nullable: false,
+                },
+                Field {
+                    field_name: "name".to_string(),
+                    field_type: "String".to_string(),
+                    nullable: false,
+                },
+                Field {
+                    field_name: "nickname".to_string(),
+                    field_type: "String".to_string(),
+                    nullable: true,
+                },
+            ],
+            primary_key: PrimaryKey {
+                field_name: "".to_string(),
+                field_type: "".to_string(),
+                is_null: true,
+            },
         };
         assert_eq!(
             template.build().unwrap(),
