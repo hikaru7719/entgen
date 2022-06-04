609abfecb0d2522c6eb047f29e3a871c38d6e368
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2022-06-04 14:22:17 +0900

    Fileのオプションを修正

--- src/template/writer.rs
+++ src/template/writer.rs
@@ -4,22 +4,18 @@ use crate::error::EntgenError;
 
 pub fn write(output_dir: &String, name: &String, body: &String) -> Result<(), EntgenError> {
     let dir = Path::new(&output_dir);
-    let path = dir.join(format!("{}.rs", name));
-    let mut file: File;
 
-    if dir.exists() && path.exists() {
-        file = File::open(path)
-            .or_else(|err| Err(EntgenError::TemplateFileOpenError(Box::new(err))))?;
-    } else if dir.exists() {
-        file = File::create(path)
-            .or_else(|err| Err(EntgenError::TemplateFileOpenError(Box::new(err))))?;
-    } else {
+    if !dir.exists() {
         std::fs::create_dir_all(dir)
             .or_else(|err| Err(EntgenError::TemplateDirCreateError(Box::new(err))))?;
-
-        file = File::create(path)
-            .or_else(|err| Err(EntgenError::TemplateFileOpenError(Box::new(err))))?;
     }
+
+    let path = dir.join(format!("{}.rs", name));
+    let mut file = File::options()
+        .create(true)
+        .write(true)
+        .open(path)
+        .or_else(|err| Err(EntgenError::TemplateFileOpenError(Box::new(err))))?;
 
     file.write(body.as_bytes())
         .or_else(|err| Err(EntgenError::TemplateFileWriteError(Box::new(err))))?;
